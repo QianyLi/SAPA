ec1eb884c6f129a81965f1a6c41f109e5c335ab4
Qianyue Li <1312869838@qq.com>
2026-08-31 13:36:57 +0800

    delete files

--- scripts/run_singleturn_sapa.sh
+++ scripts/run_singleturn_sapa.sh
@@ -15,5 +15,5 @@ python run.py \
 --end_index -1 \
 --sapa_generate "${SAPA_GENERATE:-1}" \
 --sapa_function_file "${SAPA_FUNCTION_FILE:-SAPA/output/res/function_test_res.json}" \
---sapa_param_file "${SAPA_PARAM_FILE:-SAPA/output/res/param_data_OOOOrigin_search_top2.json}" \
+--sapa_param_file "${SAPA_PARAM_FILE:-SAPA/output/res/param_data.json}" \
 --sapa_model_path "${SAPA_MODEL_PATH:?Set SAPA_MODEL_PATH to a local checkpoint}"
